728572ba375c9eeaeebfae750df2cb39ecf558c1
https://github.com/kgogo5/ <kgogo9@gmail.com>
2020-08-16 00:35:25 +0900

    리액트 초기셋팅

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "입력",
+      food: "한식",
+    };
+  }
+  render() {
+    const onSubmit = (e) => {
+      e.preventDefault();
+      axios({
+        methd: "push",
+        url: "",
+        params: {
+          id: 1,
+        },
+      });
+    };
+
+    const onChange = (props) => {
+      const val = props.target.value;
+      this.setState(() => {
+        return { text: val };
+      });
+    };
+
+    const onSelect = (props) => {
+      const sel = props.target.options[props.target.selectedIndex].text;
+      this.setState(() => {
+        return { food: sel };
+      });
+    };
+
+    return (
+      <div className="content">
+        <h1>Test</h1>
+        <form onSubmit={onSubmit}>
+          <input type="text" onChange={onChange} />
+          <p>
+            입력한 값은 <span className="final">{this.state.text}</span>입니다.
+          </p>
+
+          <h2>Select</h2>
+          <select name="sel" id="sel" onChange={onSelect}>
+            <option value="0">선택</option>
+            <option value="1">치킨</option>
+            <option value="2">피자</option>
+            <option value="3">국수</option>
+            <option value="4">냉면</option>
+            <option value="5">라면</option>
+            <option value="6">햄버거</option>
+            <option value="7">닭갈비</option>
+          </select>
+          <p>
+            선택하신 음식은 <span className="final">{this.state.food}</span>
+            입니다.
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
